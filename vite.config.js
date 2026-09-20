ac0e2da7ed52da95935654c1240276fa4a9c7b81
jssrll01 <custodiojessrell07@gmail.com>
2026-09-20 18:35:54 +0800

    Raise PWA precache limit to fit main bundle

--- vite.config.js
+++ vite.config.js
@@ -26,6 +26,7 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
         navigateFallback: '/index.html',
+        maximumFileSizeToCacheInBytes: 6 * 1024 * 1024,
       },
     }),
   ],
